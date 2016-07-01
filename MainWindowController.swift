9dea5c5fa2215a156b41a56aacf84e465c016195
Discovery Dev <adsdiscoveryteam@gmail.com>
2016-06-30 20:18:50 -0400

    Logic that shows and hides the tick marks

--- MainWindowController.swift
+++ MainWindowController.swift
@@ -15,16 +15,24 @@ class MainWindowController: NSWindowController {
     @IBOutlet weak var slider: NSSlider!
     
     @IBAction func resetControls(_ sender: NSButton) {
+        slider.numberOfTickMarks = 12
     }
     
     @IBAction func sliderState(_ sender: NSSlider) {
+        slider.numberOfTickMarks = 0
+        
         if (sender.integerValue > 50) {
             sliderStatusText.stringValue = "Slider goes up!"
         } else if (sender.integerValue < 50) {
             sliderStatusText.stringValue = "Slider goes down!"
         } else {
-            sliderStatusText.stringValue = "Slider stays the same"
+            sliderStatusText.stringValue = "Slider stays the same."
         }
+    }
+    
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
     }
     
     override var windowNibName: String? {
