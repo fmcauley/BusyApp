6e8cbcbae3109354bfc156f64e9a702f6affe330
Discovery Dev <adsdiscoveryteam@gmail.com>
2016-07-05 21:34:42 -0400

    Finished up the sample app.

--- MainWindowController.swift
+++ MainWindowController.swift
@@ -15,11 +15,27 @@ class MainWindowController: NSWindowController {
     @IBOutlet weak var sliderStatusText: NSTextField!
     @IBOutlet weak var slider: NSSlider!
     @IBOutlet weak var showTickMarks: NSButton!
-    
+    @IBOutlet weak var secrectMessage: NSSecureTextField!
+    @IBOutlet weak var revealedMessage: NSTextField!
+    @IBOutlet weak var checkBox: NSButton!
+    @IBOutlet weak var sliderWithOutTickMarks: NSButton!
     // Actions
     
-    @IBAction func resetControls(_ sender: NSButton) {
+    @IBAction func revelTheMessage(_ sender: NSButton) {
+        if secrectMessage.stringValue.characters.count > 0 {
+            revealedMessage.stringValue = secrectMessage.stringValue
         }
+    }
+    
+    @IBAction func resetControls(_ sender: NSButton) {
+        secrectMessage.stringValue = ""
+        revealedMessage.stringValue = ""
+        checkBox.state = NSOnState
+        slider.integerValue = 50
+        slider.numberOfTickMarks = 9
+        sliderWithOutTickMarks.state = NSOnState
+        sliderStatusText.stringValue = "Slider stays the same."
+    }
     
     @IBAction func sliderState(_ sender: NSSlider) {
         if (sender.integerValue > 50) {
@@ -33,9 +49,9 @@ class MainWindowController: NSWindowController {
     
     @IBAction func toggleTickers(_ sender: NSButton) {
         if sender.tag == 0 {
-            slider.numberOfTickMarks = 0
-        } else {
             slider.numberOfTickMarks = 9
+        } else {
+            slider.numberOfTickMarks = 0
         }
         
     }
@@ -56,6 +72,7 @@ class MainWindowController: NSWindowController {
     
     override func windowDidLoad() {
         super.windowDidLoad()
+         slider.numberOfTickMarks = 9
 
     }
         
